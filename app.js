b9fd68471a728e4782972e724656e15f6f02d576
CristopherSandoval <christophersandoval1501@gmail.com>
2022-05-05 21:53:13 -0500

    Primera parte express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Holaa a todos omeu');
+})
+//Iniciamos la app
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
