ae7fa3c97e0e35659c800c13eae1089dfab3956a
CristopherSandoval <christophersandoval1501@gmail.com>
2022-05-05 21:57:03 -0500

    agregando una nueva ruta launchx

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require("express");
 
 const app = express()
 const port = 3000
-
+//Aquí se les da un url por ejemplo el de abajo launchx.
 app.get('/', (req, res) => {
     res.send('Holaa a todos omeu');
 })
 //Iniciamos la app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
+})
+//Agregando una nueva ruta
+//respondiendo texto
+app.get('/launchx', (req, res) => {
+    res.send('Estoy en launchx!');
 })
